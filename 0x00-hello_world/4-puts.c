7467848fe0542d779296d6fdfe7ea52e6275dc23
Edoma .O. Benjamin <oghosabenjamin@gmail.com>
2022-10-14 18:00:58 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - prints the phrase "Programming is like building a multilingual puzzle
@@ -13,9 +11,7 @@
 int main(void)
 
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
