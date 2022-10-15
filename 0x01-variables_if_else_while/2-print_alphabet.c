005005699c620c1f0f91b4ba258a248c5732ff42
Edoma .O. Benjamin <oghosabenjamin@gmail.com>
2022-10-15 20:10:56 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,14 @@
  * Return: always return 0
  */
 
-int main (void)
+int main(void)
 {
-	char ch = 'a';
-	
-	while(ch <= 'z')
+	char ch = 'a';	
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
