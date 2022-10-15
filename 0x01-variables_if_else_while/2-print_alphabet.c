461b92099c1a52752c2d322e6cfbd9c3baae594a
root <Boogie2k>
2022-10-15 11:56:45 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets
+ * alphabets are looped using the while loop
+ * Return: always return 0
+ */
+
+int main (void)
+{
+	char ch = 'a';
+	while(ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return 0;
+}
