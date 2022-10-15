ac08bef3271dc2cea02d32175990df8795f1c05e
root <Boogie2k>
2022-10-15 13:24:22 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - prints the numbers
+ * numbsers are looped using the for loop
+ * return: always return 0
+ */
+
+int main (void)
+{
+int i;
+	for (i = 0; i<10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
