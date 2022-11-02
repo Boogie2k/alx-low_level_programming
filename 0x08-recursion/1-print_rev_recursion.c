743d54dcbceac64fe4f9a4bb82360e83ba469990
Edoma .O. Benjamin <oghosabenjamin@gmail.com>
2022-11-03 00:30:44 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,29 +1,14 @@
 #include "main.h"
 
 /**
- * getEnd - gets the end of a string;
- * @s: string to be reversed
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
  */
-
-void getEnd(char *s)
-{
-	if (*s != '\0')
-	{
-		s++;
-		getEnd(s);
-	}
-	_putchar(*s);
-}
-
-/**
- * _print_rev_recursion - prints a string in reverse using recursion
- * @s: string in question
- */
-
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-		getEnd(s);
-	else
-		_putchar('\n');
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
