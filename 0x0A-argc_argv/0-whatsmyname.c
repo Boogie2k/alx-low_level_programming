a02bad7b138856900e5309734b2a7afd294a694d
Boogie2k <oghosabenjamin@gmail.com>
2022-11-08 11:43:17 +0100

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints its name followed by a new line
+ * @argc: counter
+ * @argv: array of arguments
+ * Return: 0
+ */
+
 int main (int argc, char* argv[]){
 int i;
 	for(i = 0; i < argc; i++){
