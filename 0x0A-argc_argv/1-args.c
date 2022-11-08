126ef0fd5828480a75f33a04cff7bbf581a32f26
Boogie2k <oghosabenjamin@gmail.com>
2022-11-08 12:07:14 +0100

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * argc - counts the number of argument
+ * argv - contains the argument in the array
+ * return 0 - terminates the program
+ */
+
+int main (int argc, char *argv[]){
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%d\n", i+1);
+	}
+
+	return (0);
+}
+
