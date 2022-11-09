2c06478d9685181883880e127cef72b7b76c4f64
Edoma .O. Benjamin <oghosabenjamin@gmail.com>
2022-11-09 22:08:01 +0100

    Create 1-strncat.c

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * **@dest: pointer to the destination array
+ * **@src: the string to be appended
+ * *@n: maximum number of characters to be appended.
+ * *Return: concantenet string
+ */
+
+char *_strncat(char *dest, char *src, int n);
