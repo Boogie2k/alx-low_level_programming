a1c1784f750eae07ebcc66346a90b0dad7208aa2
Edoma .O. Benjamin <oghosabenjamin@gmail.com>
2022-11-09 22:11:57 +0100

    Create 4-strpbrk.c

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - checks for a character in a string
+ * *@s: A pointer to the string being checked for a character
+ * *@accept: the checking character
+ * *
+ * *Return: A pointer a value or null if there is a match or not
+ */
+
+char *_strpbrk(char *s, char *accept);
