5013859433a4536f9c4f5e2fc362605647d7b730
Edoma .O. Benjamin <oghosabenjamin@gmail.com>
2022-11-09 22:11:07 +0100

    Create 3-strspn.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strspn - gets the length of a prefix substring
+ * *@s: string to be checked
+ * *@accept: string to compare to s
+ * *
+ * *Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept);
