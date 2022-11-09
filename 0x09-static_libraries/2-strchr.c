686fc222dbe30efbfc36cc19c9f1bb8805591dd0
Edoma .O. Benjamin <oghosabenjamin@gmail.com>
2022-11-09 22:26:46 +0100

    Create 2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_strchr- checks for a character in a string
+ * *@s:A pointer to the string checking for a character
+ * *@c: the checked character
+ * *
+ * *Return: A pointer a value or null
+ */
+
+char *_strchr(char *s, char c);
