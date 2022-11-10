a5fa8489d80767aa3bc1ea03864beb0c03b352d5
Edoma .O. Benjamin <oghosabenjamin@gmail.com>
2022-11-10 19:38:37 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
- * malloc_checked - reserves space for an array
- * @b: value
- * Return: ptr or value 98
+ * malloc_checked - allocate memory
+ * exit - exit status for normal process termination
+ * @b: value to store in memory
+ *
+ * Return: pointer to allocated memory, exit status 98 if fail
  */
-
 void *malloc_checked(unsigned int b)
 {
-	int *ptr;
+	int *arr;
 
-	ptr = malloc(sizeof(b));
-	if (!ptr)
-	{
-		exit(98);
-	}
-	return (ptr);
+	/* allocate memory to arr */
+	arr = malloc(b);
+
+	/* if malloc fails */
+	if (arr == NULL)
+		exit(98); /* exit status */
+
+	return (arr);
 }
